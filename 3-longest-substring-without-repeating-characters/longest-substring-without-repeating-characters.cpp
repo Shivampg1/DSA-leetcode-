007bb2980a6430500330745048fdaa60d8a8619c
Shivampg1 <147384496+Shivampg1@users.noreply.github.com>
2025-07-18 12:00:11 +0530

    Time: 19 ms (30.51%) | Memory: 14.4 MB (22.62%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n=s.size();
-        set<char> seen;
+        unordered_set<char> seen;
         int max_length=0;
         int left=0;
         for(int right=0;right<n;right++){
