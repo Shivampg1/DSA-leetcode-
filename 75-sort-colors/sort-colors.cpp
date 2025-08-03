2591488b24d52ef4838a094025f029d7e2186fae
Shivampg1 <147384496+Shivampg1@users.noreply.github.com>
2025-08-03 22:06:55 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (11.99%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        vector<int> ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+           ans.push_back(nums[i]);
+        }
+      //return -1;
+    }
+};
