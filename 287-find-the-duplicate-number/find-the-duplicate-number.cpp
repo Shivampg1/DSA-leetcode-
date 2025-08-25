4f5281bef4187781ef7252ae5902346f804a8650
Shivampg1 <147384496+Shivampg1@users.noreply.github.com>
2025-08-25 16:17:38 +0530

    Time: 39 ms (24.87%) | Memory: 65.1 MB (70.04%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -5,7 +5,7 @@ public:
         int i=0;
         int j=1;
 
-        while(i<nums.size()&&j<nums.size()){
+        while(j<nums.size()){
             if(nums[i]!=nums[j]){
                 i=j;
                 j++;   
