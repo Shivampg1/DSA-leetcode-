f32b834aeff0c673244ccfbf81ac493a0ac1e259
Shivampg1 <147384496+Shivampg1@users.noreply.github.com>
2025-09-26 16:17:41 +0530

    Time: 1591 ms (6.52%) | Memory: 16.6 MB (42.88%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        //vector<int>ans;
+
         int n=nums.size();
         int count=0;
+
+
        for (int i = 0; i < n; i++) {           
         int j = i + 1;
         int k = j + 1;
