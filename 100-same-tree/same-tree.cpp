2be808fd10fe08eca3512a2205f66a765a3fa547
Shivampg1 <147384496+Shivampg1@users.noreply.github.com>
2025-11-25 23:06:03 +0530

    Update same-tree.cpp

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -19,7 +19,7 @@ public:
         return false;
       }
       if(p->val==q->val){
-        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right); 
      
       }
       else{
