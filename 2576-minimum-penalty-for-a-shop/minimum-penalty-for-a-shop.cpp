0a4b87a6443482986aabcac26d01efd143cd0986
Shivampg1 <147384496+Shivampg1@users.noreply.github.com>
2025-12-26 21:41:08 +0530

    Time: 3 ms (89.07%) | Memory: 13.3 MB (91.48%) - LeetSync

---
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+       int bestClosingTime(string s) {
+        int pen = 0;
+        for (char c : s) if (c == 'Y') pen++;
+
+        int best = pen, ans = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'Y') pen--;
+            else pen++;
+
+            if (pen < best) {
+                best = pen;
+                ans = i + 1;
+            }
+        }
+        return ans;
+    }
+};
