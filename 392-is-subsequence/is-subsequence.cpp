eefd2a388da4d206ac9970cf9a29db86c24e6566
Shivam Choubey <147384496+Shivampg1@users.noreply.github.com>
2026-04-06 19:15:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (66.05%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -4,17 +4,32 @@ public:
         int n=s.size();
         int m=t.size();
 
-      vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-      for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            if(s[i-1]==t[j-1]){
-                dp[i][j]=1+dp[i-1][j-1];
-            }
-            else{
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
+    //   vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+    //   for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=m;j++){
+    //         if(s[i-1]==t[j-1]){
+    //             dp[i][j]=1+dp[i-1][j-1];
+    //         }
+    //         else{
+    //             dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+    //         }
+    //     }
+    //   }
+    //   return dp[n][m]==n;
+    // }
+
+    int i=0;
+    int j=0;
+    while(i<n && j<m){
+        if(s[i]==t[j]){
+            i++;
         }
-      }
-      return dp[n][m]==n;
+        j++;
+    }
+    if(i==n){
+        return true;
+    }
+
+    return false;
     }
 };
