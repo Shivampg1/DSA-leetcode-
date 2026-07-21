4aa0fba6368111deb1147b8b9baa287e486321d0
Shivam Choubey <147384496+Shivampg1@users.noreply.github.com>
2026-07-21 23:40:29 +0530

    Update reordered-power-of-2.cpp

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -5,7 +5,7 @@ public:
             string s = to_string(x);
             sort(s.begin(), s.end());
             return s;
-        };
+        }; 
         string target = countDigits(n);
         for (int i = 0; i < 31; i++) {
             if (countDigits(1 << i) == target) return true;
